89b94dded5b521b9612b90f31ab74e3b13ceea94
Your Name <timofey695@gmail.com>
2021-03-03 15:46:50 +0300

    firt page

--- script.js
+++ script.js
@@ -1,26 +1,96 @@
 'use strict'
 const users = [
-    ['sadaw@abc.com', 'qwerty']
+    {
+        email: "timofey695@gmail.com",
+        password: 'qwerty'
+    },
+    {
+        email: "abd@gma",
+        password: 'asdf'
+    },
+    {
+        email: "abc@gma",
+        password: 'zxc'
+    }
 ]
-butt1.onclick = function () {
+const inputEmail = document.getElementById('login-input')
+const inputPass = document.getElementById('password-input')
+
+const inputEmailReg = document.getElementById('login-input-reg')
+const inputPassReg = document.getElementById('password-input-reg')
+const inputCPass = document.getElementById('cp')
+
+inputEmail.addEventListener('change', function () {
+    if (!(inputEmail.value === '' || inputPass.value === '')) {
+        buttLog.removeAttribute('disabled')
+
+    } else {
+        buttLog.setAttribute('disabled', true)
+    }
+})
+inputPass.addEventListener('change', function () {
+    if (!(inputEmail.value === '' || inputPass.value === '')) {
+        buttLog.removeAttribute('disabled')
+    } else {
+
+        buttLog.setAttribute('disabled', true)
+    }
+})
+
+
+inputEmailReg.addEventListener('change', function () {
+    if (!(inputEmailReg.value === '' || inputPassReg.value === '' || inputCPass.value === '')) {
+        buttReg.removeAttribute('disabled')
+
+    } else {
+        buttReg.setAttribute('disabled', true)
+    }
+})
+inputPassReg.addEventListener('change', function () {
+    if (!(inputEmailReg.value === '' || inputPassReg.value === '' || inputCPass.value === '')) {
+        buttReg.removeAttribute('disabled')
+    } else {
+
+        buttReg.setAttribute('disabled', true)
+    }
+})
+inputCPass.addEventListener('change', function () {
+    if (!(inputEmailReg.value === '' || inputPassReg.value === '' || inputCPass.value === '')) {
+        buttReg.removeAttribute('disabled')
+    } else {
+
+        buttReg.setAttribute('disabled', true)
+    }
+})
+
+
+buttLog.onclick = function () {
     let login = document.getElementById('login-input').value
     let password = document.getElementById('password-input').value
-    if (login == false || password == false) {
-        butt1.setAttribute("disabled", true)
-    }
+    let validLogin = 0;
+    let validPass = 0;
     for (let i = 0; i < users.length; i++) {
-        if (users[i][0] !== login) {
-            alert('неверный логин')
-            break
+        if (users[i].email !== login) {
+            continue
         }
-        if (users[i][1] !== password) {
+        if (users[i].password !== password) {
+            validPass += 1
             alert('неверный пароль')
             break
         } else {
             alert('добро пожаловать')
+            validLogin += 1
+            const main = document.getElementsByClassName('menu')
+            main[0].style.display = "flex"
+            let log = document.getElementsByClassName('login')
+            log[0].style.display = 'none'
             break
         }
 
+    }
+    if (validPass !== 0) {
+    } else if (validLogin == 0) {
+        alert('неверный логин')
     }
 }
 function registration() {
@@ -39,28 +109,37 @@ let k = function login() {
     log[0].style.visibility = 'visible'
     log[0].style.position = 'static'
 }
-butt2.onclick = function () {
+buttReg.onclick = function () {
     let login = document.getElementById('login-input-reg').value
-    let password = document.getElementById('password-input-reg').value
+    let passw = document.getElementById('password-input-reg').value
     let cp = document.getElementById('cp').value
     console.log(login)
-    console.log(password)
+    console.log(passw)
     console.log(cp)
-    for (let i = 0; i < users.length; ++i) {
-        if (users[i][0] == login) {
+    let validLogin = 0
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email == login) {
+            validLogin += 1
+            console.log('kek')
             alert('Существующий логин')
             break
         }
-        if (users[i][1] == password) {
-            alert('Существующий пароль')
-            break;
+    }
+    if (validLogin == 0) {
+        if (passw == cp) {
+            users.push({
+                email: login,
+                password: passw
+            })
+            console.log(users)
         }
+    }
+}
 
-    }
-    if (password == cp) {
-        users.push([login, password])
-        console.log(users)
-        k()
-    }
+function back() {
+    const main = document.getElementsByClassName('menu')
+    main[0].style.display = "none"
+    let log = document.getElementsByClassName('login')
+    log[0].style.display = 'flex'
 }
 
